db91d0d6d177e8379c089c9eb45cb051930c2071
soumyarepo <ranjan.soumya8055@gmail.com>
2024-02-14 16:16:01 +0530

    1st commit

---
+++ myapp.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const app = express();
+const PORT = 3000;
+
+let data = { message: "Hello, Autozone" };
+
+app.get('/', (req, res) => {
+  res.json(data);
+});
+
+// Load dummy certificates
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server running on https://localhost:${PORT}`);
+});
